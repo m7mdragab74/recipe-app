c48e483d573d61656fd915d25a5cd351a5b6dbe8
m7mdragab <mr341349@gmail.com>
2024-08-17 16:21:37 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:recipe_app/views/home_page.dart';
+
+void main() {
+  runApp(const RecipeApp());
+}
+
+class RecipeApp extends StatelessWidget {
+  const RecipeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
