92383f4d9de5fe54467c4bff47c1ec4fac5c701a
m7mdragab <mr341349@gmail.com>
2024-08-19 19:04:39 +0300

    work on ui

--- lib/views/recipe_list.dart
+++ lib/views/recipe_list.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_app/models/image_model.dart';
 import 'package:recipe_app/views/favorite_recipe.dart';
 import 'package:recipe_app/widget/custom_app_bar.dart';
 import 'package:recipe_app/widget/custom_botton_nav_bar.dart';
+import 'package:recipe_app/widget/image_card.dart';
 
 class RecipesListPage extends StatefulWidget {
   const RecipesListPage({super.key});
@@ -15,6 +17,12 @@ class RecipesListPage extends StatefulWidget {
 class _RecipesListPageState extends State<RecipesListPage> {
   int _selectedIndex = 0;
 
+  final List<ImageModel> imageView = [
+    ImageModel(image: 'assets/IMG_3667.HEIC'),
+    ImageModel(image: 'assets/IMG_3667.HEIC'),
+    ImageModel(image: 'assets/IMG_3667.HEIC'),
+    ImageModel(image: 'assets/IMG_3667.HEIC'),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,6 +42,21 @@ class _RecipesListPageState extends State<RecipesListPage> {
       appBar: const CustomAppBarWidget(
         title: 'Recipes List',
       ),
+      body: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 9,
+            mainAxisSpacing: 16,
+          ),
+          itemCount: imageView.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.all(12),
+              child: ImageCard(
+                image: imageView[index],
+              ),
+            );
+          }),
     );
   }
 }
---
+++ lib/widget/image_card.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:recipe_app/models/image_model.dart';
+
+class ImageCard extends StatefulWidget {
+  ImageCard({super.key, required this.image});
+  final ImageModel image;
+
+  @override
+  _ImageCardState createState() => _ImageCardState();
+}
+
+class _ImageCardState extends State<ImageCard> {
+  bool isFavorite = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(18),
+          child: Image.asset(
+            widget.image.image,
+            fit: BoxFit.cover,
+            height: 200,
+            width: 175,
+          ),
+        ),
+        Positioned(
+          top: 8,
+          right: 8,
+          child: Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.white.withOpacity(0.5),
+            ),
+            child: IconButton(
+              onPressed: () {
+                setState(() {
+                  isFavorite = !isFavorite;
+                });
+              },
+              icon: Icon(
+                isFavorite ? Icons.favorite : Icons.favorite_border,
+                color: isFavorite ? Colors.red : Colors.white,
+                size: 30,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
