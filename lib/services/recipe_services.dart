f23c3414322a726896d7fb47ed630c9a39ee3402
m7mdragab <mr341349@gmail.com>
2024-08-21 09:55:18 +0300

    create recipe service

--- lib/services/recipe_services.dart
+++ lib/services/recipe_services.dart
@@ -1,7 +1,28 @@
 import 'package:dio/dio.dart';
+import 'package:recipe_app/models/recipe_model.dart';
 
 class RecipeService {
   final Dio dio;
 
   RecipeService(this.dio);
+
+  Future<List<RecipeModel>> getRecipes() async {
+    try {
+      Response response = await dio.get('path');
+
+      Map<String, dynamic> jsonData = response.data;
+
+      List<dynamic> recipes = jsonData['recipes'];
+
+      List<RecipeModel> recipeList = [];
+
+      for (var recipes in recipes) {
+        RecipeModel recipeModel = RecipeModel.fromJson(recipes);
+        recipeList.add(recipeModel);
+      }
+      return recipeList;
+    } catch (e) {
+      return [];
+    }
+  }
 }
